e3fe76eab27b470cd2f1e98cb028191fdf84d0ea
pcabillo558845-netizen <p.cabillo.558845@umindanao.edu.ph>
2026-02-26 21:07:38 +0800

    Add files via upload

---
+++ pdo_lab/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "pdo_lab";
+$username = "root";
+$password = "";
+ 
+$pdo = new PDO(
+    "mysql:host=$host;dbname=$dbname",
+    $username,
+    $password
+);
+ 
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
